f0f898a3267671f3c1eb64a4ea1dd29ca11846f8
Buck Golemon <buck@yelp.com>
2012-01-26 18:26:40 -0800

    refactor common Node functionality

--- refactorlib/cheetah/node.py
+++ refactorlib/cheetah/node.py
@@ -1,29 +1,16 @@
+"""
+cheetah-specific additions to the lxml element node class.
+"""
 from lxml import etree
+from refactorlib.node import RefactorLibNodeBase
 
-class CheetahNodeBase(etree.ElementBase):
+class CheetahNodeBase(RefactorLibNodeBase):
 	def find_calls(self, func_name):
 		return self.xpath(
 			'.//Placeholder'
 			'[./CheetahVarNameChunks/CallArgString]'
 			'[./CheetahVarNameChunks/DottedName="%s"]' % func_name
 		)
-
-	def remove_call(self):
-		raise NotImplementedError('remove_call')
-
-	def replace_self(self, other):
-		parent = self.getparent()
-		parent.replace(self, other)
-
-	def remove_self(self):
-		parent = self.getparent()
-		parent.remove(self)
-	
-	def totext(self):
-		return etree.tostring(self, method='text')
-
-	def tostring(self, method=None):
-		return etree.tostring(self, method=method)
 
 class CheetahPlaceholder(CheetahNodeBase):
 	def remove_call(self):
--- refactorlib/node.py
+++ refactorlib/node.py
@@ -1,2 +1,25 @@
-# Some basic additions to the lxml element node class.
+"""
+Some basic additions to the lxml element node class.
+"""
+from lxml import etree
 
+class RefactorLibNodeBase(etree.ElementBase):
+	def remove_call(self):
+		raise NotImplementedError('remove_call')
+
+	def replace_self(self, other):
+		parent = self.getparent()
+		parent.replace(self, other)
+
+	def remove_self(self):
+		parent = self.getparent()
+		parent.remove(self)
+	
+	def totext(self):
+		return etree.tostring(self, method='text')
+
+	def tostring(self, method=None):
+		return etree.tostring(self, method=method)
+
+
+__all__ = ('RefactorLibNodeBase',)
